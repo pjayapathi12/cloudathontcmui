68c6c4410d58a5bd95b23f84b6b6624b0c4e5e22
Linda <lchen.59@gmail.com>
2022-02-26 04:10:53 +0000

    Update styling

--- src/app/error-table/error-table.component.ts
+++ src/app/error-table/error-table.component.ts
@@ -8,13 +8,35 @@ import { DetailByEnv, ErrorDetails, ErrorView, ErrorRow } from '../models/ErrorS
 })
 export class ErrorTableComponent implements OnInit {
   @Input() errorData: ErrorRow[];
+  closedJiraStatuses: string[] = [ 'Closed', 'Completed' ];
 
   constructor() {
     
   }
 
   ngOnInit(): void {
-    
+    console.log('before checkForIssues', this.errorData);
+    // this.checkForIssues();
+  }
+
+  checkForIssues() {
+    if (this.errorData) {
+      this.errorData.forEach(function(error, index) {
+        if (error.isNetNew) {
+          if (this.closedJiraStatuses.indexOf(error.jiraStatus) == -1) {
+            this.errorData[index].isFlagged = true;
+            this.errorData[index].issueDescription = 'New Error - JIRA not closed';
+          } else {
+            this.errorData[index].isFlagged = true;
+            this.errorData[index].issueDescription = 'New Error - JIRA closed';
+          }
+        }
+        else if (!error.jira) {
+          this.errorData[index].isFlagged = true;
+          this.errorData[index].issueDescription = 'JIRA missing';
+        }
+      });
+    }
   }
 
 }
